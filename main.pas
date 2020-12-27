90667af557a738c2b7086fb02243fb7cd17daaaa
kkm <surdej@devanddeliver.com>
2020-12-27 17:18:31 +0100

    Init of my pain

---
+++ main.pas
@@ -0,0 +1,47 @@
+Program RandomSound;
+
+Uses SysUtils;
+
+Var Info : TSearchRec;
+    Count : LongInt;
+    ArrCount : LongInt;
+    FilesArr : Array[0..999] of string;
+    FileName : string;
+    TrackedFile : Int64;
+    FileId : Longint;
+    RandomFileName : string;
+    OutputFile : Array[0..5000] of byte;
+
+Begin
+  Count:=0;
+  ArrCount:=0;
+  If FindFirst ('sounds/*',faAnyFile and faDirectory,Info)=0 then
+    begin
+    Repeat
+      Inc(Count);
+      With Info do
+        begin
+          FileName := Format('%s', [Name]);
+          TrackedFile := (CompareText(FileName, '.') + CompareText(FileName, '..') + CompareText(FileName, '.gitignore'));
+          If (TrackedFile > 2) then
+          begin
+            FilesArr[ArrCount] := Name;
+            Inc(ArrCount)
+          end
+        end;
+    Until FindNext(info)<>0;
+    end;
+  FindClose(Info);
+  
+  Count:=0;
+  randomize;
+
+  begin
+  Repeat
+    FileId := random(ArrCount);
+    RandomFileName := FilesArr[FileId];
+    Inc(Count);
+    writeln(RandomFileName);
+  Until (Count > 9)
+  end
+End.
